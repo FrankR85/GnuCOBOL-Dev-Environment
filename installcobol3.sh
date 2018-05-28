bb788e1b0c42b0d82e6a26ff2cf1b85be80b24e2
Frank Rehfeldt <frank@internetschach.de>
2018-05-28 20:36:58 +0200

    change target directory for extracting GnuCOBOL

--- installcobol3.sh
+++ installcobol3.sh
@@ -1,17 +1,15 @@
 sudo apt-get update
-sudo apt-get install -y --force-yes libgmp3-dev libdb-dev automake flex bison help2man texinfo
+sudo apt-get install -y --force-yes libgmp3-dev libdb-dev
 
 echo "download GnuCOBOL"
-mkdir /vagrant/gnucobol3
-mkdir /vagrant/gnucobol3/latest
-wget  -O /vagrant/gnucobol3/latest.tar.gz "https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.gz/download"
-cd /vagrant/gnucobol3
-tar xvf latest.tar.gz -C /vagrant/gnucobol3/latest --strip-components=1
+mkdir /home/vagrant/gnucobol3
+mkdir /home/vagrant/gnucobol3/latest
+wget  -O /home/vagrant/gnucobol3/latest.tar.gz "https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.xz/download"
+cd /home/vagrant/gnucobol3
+tar xvf latest.tar.gz -C /home/vagrant/gnucobol3/latest --strip-components=1
 cd latest/
 
 ./configure
-aclocal
-automake
 make
 sudo make install
 sudo ldconfig
