01cdbfa6877aff26a3af6d56d6cf560f608e776b
Frank Rehfeldt <frank@internetschach.de>
2018-05-30 21:34:11 +0200

    change download URL to official gnu mirror

--- installcobol3.sh
+++ installcobol3.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y --force-yes libgmp3-dev libdb-dev
 echo "download GnuCOBOL"
 mkdir /home/vagrant/gnucobol3
 mkdir /home/vagrant/gnucobol3/latest
-wget  -O /home/vagrant/gnucobol3/latest.tar.gz "https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.xz/download"
+wget  -O /home/vagrant/gnucobol3/latest.tar.gz "http://gnu.c3sl.ufpr.br/alpha/gnucobol/gnucobol-3.0-rc1.tar.xz"
 cd /home/vagrant/gnucobol3
 tar xvf latest.tar.gz -C /home/vagrant/gnucobol3/latest --strip-components=1
 cd latest/
