f96c583de2498f9c142212c7878cc3e126a6c2cf
Frank Rehfeldt <frank@internetschach.de>
2018-05-31 18:48:11 +0200

    remove target resources

--- cobol/fizzbuzz/run-ut.sh
+++ cobol/fizzbuzz/run-ut.sh
@@ -1,4 +1,4 @@
 /vagrant/cobol-unit-test/run-ut -d -p fizzbuzz FIZZBUZC FIZZBUZZ FIZZBUZT
 
-touch /vagrant/cobol/fizzbuzz/target/FIZZBUZZ
+#touch /vagrant/cobol/fizzbuzz/target/FIZZBUZZ
 cobc -x -std=ibm /vagrant/cobol/fizzbuzz/src/FIZZBUZZ.CBL -o /vagrant/cobol/fizzbuzz/target/FIZZBUZZ
